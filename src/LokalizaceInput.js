afa4a5222adbcc616a1a029c04ef6c79fb94fc11
Martin Povolny <mpovolny@redhat.com>
2020-09-02 22:02:02 +0200

    Work on lokalizace/cast obce.

--- src/LokalizaceInput.js
+++ src/LokalizaceInput.js
@@ -1,17 +1,41 @@
 import React from "react";
 
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import FormControl from "@material-ui/core/FormControl";
 import Grid from "@material-ui/core/Grid";
 import MenuItem from "@material-ui/core/MenuItem";
+import TextField from '@material-ui/core/TextField';
 
 import AsyncAutocomplete from "./AsyncAutocomplete";
 //import useStyles from "./useStyles";
 
+const CastObceInput = props => {
+	const {
+		options
+	} = props;
+
+	return (
+    <Autocomplete
+      id="combo-box-demo"
+      options={options}
+      getOptionLabel={(option) => option}
+      style={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Část obce" variant="outlined" />}
+    />
+	);
+};
+
 const LokalizaceInput = props => {
-  //const classes = useStyles();
   const {
     onChange
   } = props;
+
+  // FIXME: dynamicky nacist
+  const [castiObce, setCastiObce] = React.useState(['tam', 'jinde']);
+
+  const onChangePart = () => {
+    alert('onChangePart');
+  };
 
   return (
     <FormControl>
@@ -20,7 +44,7 @@ const LokalizaceInput = props => {
 					<AsyncAutocomplete onChange={onChange} />
 				</Grid>
         <Grid item>
-          Lokalizace
+					<CastObceInput onChange={onChangePart} options={castiObce} />
         </Grid>
         <Grid item>Lokalizace</Grid>
       </Grid>
